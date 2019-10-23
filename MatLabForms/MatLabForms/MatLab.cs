e74904a1bd1a6d0ecfd537792d06c924a54bc8be
uniqueUserNameee <51269536+uniqueUserNameee@users.noreply.github.com>
2019-10-23 04:59:16 +0300

    Начал создавать окно для работы пользователя

---
+++ MatLabForms/MatLabForms/MatLab.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatLabForms
+{
+    public partial class MatLab : Form
+    {
+        public MatLab()
+        {
+            InitializeComponent();
+        }
+
+        private void MatLab_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
