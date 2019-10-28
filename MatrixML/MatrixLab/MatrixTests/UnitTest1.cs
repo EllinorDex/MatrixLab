5a025091635a9498cdd450919404e739e30f1bf5
elldex <ellinordex@gmail.com>
2019-10-28 20:10:57 +0300

    То самое, общее решение. Где 3 проекта

---
+++ MatrixML/MatrixLab/MatrixTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MatrixTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
