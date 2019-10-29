de016bd21cc1f1a5e31600f9584a45890c0a9b6d
elldex <ellinordex@gmail.com>
2019-10-29 07:00:35 +0300

    Попробовал протеститоровать sum

--- MatrixML/MatrixLab/MatrixLib/Sum.cs
+++ MatrixML/MatrixLab/MatrixLib/Sum.cs
@@ -12,12 +12,24 @@ namespace MatrixLib
         private Matrix _matrixLeft;
         private Matrix _matrixRight;
 
+        public Matrix MatrixLeft
+        {
+            get { return _matrixLeft; }
+            set { _matrixLeft = value; }
+        }
+
+        public Matrix MatrixRight
+        {
+            get { return _matrixRight; }
+            set { _matrixRight = value; }
+        }
+
         public Matrix GetResult()
         {
             operationsmatlab op = new operationsmatlab();
             Converter converter = new Converter();
             MWArray[] res = op.Sum(1, converter.ConvertFromMatrixToMLMatrix(_matrixLeft), converter.ConvertFromMatrixToMLMatrix(_matrixRight));
-            int[,] resArr = converter.ConvertFromMLMatrixToMatrix(res);
+            int[,] resArr = converter.ConvertFromMLMatrixToMatrix(res[0]);
             return new Matrix(_matrixLeft.CountOfRows, _matrixLeft.CountOfColumns, resArr);
         }
     }
--- MatrixML/MatrixLab/MatrixTest/TestMatrix.cs
+++ MatrixML/MatrixLab/MatrixTest/TestMatrix.cs
@@ -58,5 +58,22 @@ namespace MatrixTest
             //for (int i = 0, j = 0; i < A.CountOfColumns && j < A.CountOfRows; ++i, ++j)
             //    Assert.AreEqual(0, A[i, j]);
         }
+
+        [TestMethod]
+        public void UseSumMatrix()
+        {
+            uint lengthCol = 5;
+            uint lengthRow = 7;
+            Matrix A = new Matrix(lengthRow, lengthCol, "zeros");
+            Matrix B = new Matrix(lengthRow, lengthCol, "zeros");
+
+            Sum sum = new Sum();
+            sum.MatrixLeft = A;
+            sum.MatrixRight = B;
+            Matrix C = sum.GetResult();
+
+            for (int i = 0, j = 0; i < C.CountOfColumns && j < C.CountOfRows; ++i, ++j)
+                Assert.AreEqual(0, C[i, j]);
+        }
     }
 }
