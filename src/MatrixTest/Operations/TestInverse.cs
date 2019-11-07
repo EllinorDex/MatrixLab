1888a62dd815ec276bcdc3d73e028923f16a3744
jomborus <jomborus@gmail.com>
2019-11-08 01:45:31 +0300

    сдалал, чтобы git сохранял packages, тесты дописал, но повторно не запускал. на досуге допишу. Саня установи VS19

--- src/MatrixTest/Operations/TestInverse.cs
+++ src/MatrixTest/Operations/TestInverse.cs
@@ -1,10 +1,60 @@
 ﻿using MatrixLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using Moq;
 
 namespace MatrixTest.Operations
 {
-    class TestInverse
+    [TestClass]
+    public class TestInverse
     {
+        [TestMethod]
+        public void InverseOnesMatrix()
+        {
+
+            Matrix A = new Matrix((uint)5, (uint)5, MatrixType.ones);
+
+            InverseMatrix Matr = new InverseMatrix(A);
+
+            Matrix Inverse = Matr.Calculate();
+
+            CollectionAssert.AreEqual(A.Get2DArray(), Inverse.Get2DArray());
+        }
+
+        [TestMethod]
+        public void InverseUsersMatrix0()
+        {
+            int[,] matrOfArray = new int[,] { { 2, 6, 5 }, { 5, 3, -2 }, { 7, 4, -3 } };
+            int[,] resultMatr = new int[,] { { 1, -38, 27 }, { -1, 41, -29 }, { 1, -34, 24 } };
+            Matrix A = new Matrix((uint)3, (uint)3, matrOfArray);
+
+            InverseMatrix Matr = new InverseMatrix(A);
+
+            Matrix Inverse = Matr.Calculate();
+
+            CollectionAssert.AreEqual(resultMatr, Inverse.Get2DArray());
+        }
+        [TestMethod]
+        public void InverseUsersMatrixNumb()
+        {
+            int[,] matrOfArray = new int[,] { { 0, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] resultMatr = new int[,] { { }, { }, { } };
+            Matrix A = new Matrix((uint)3, (uint)3, matrOfArray);
+
+            InverseMatrix Matr = new InverseMatrix(A);
+
+            Matrix detem = Matr.Calculate();
+
+            Assert.AreEqual(3, detem);
+        }
+
+        [TestMethod]
+        public void InverseException()
+        {
+            int[,] matrOfArray = new int[,] { { 0, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 7, 8, 9 } };
+            Matrix A = new Matrix((uint)4, (uint)3, matrOfArray);
+
+            Assert.ThrowsException<MatrixException>(() => new InverseMatrix(A));
+        }
     }
 }
