1d6e84d3084bea570ff58d3f0b25d444933767e6
elldex <ellinordex@gmail.com>
2019-11-15 23:03:04 +0300

    Забыл один тест - вот он

--- src/MatrixTest/Operations/TestInverse.cs
+++ src/MatrixTest/Operations/TestInverse.cs
@@ -66,7 +66,9 @@ namespace MatrixTest.Operations
         {
             int[,] matrOfArray = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
             Matrix A = new Matrix((uint)3, (uint)3, matrOfArray);
-            Assert.ThrowsException<MatrixException>(() => new InverseMatrix(A));
+            InverseMatrix IM = new InverseMatrix(A);
+
+            Assert.ThrowsException<MatrixException>(() => IM.Calculate());
         }
     }
 }
